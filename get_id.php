82a1d9b1c86e8bf0bcf5a25168d32c5c1802af00
Jhowg14 <jhonatan.lourenco@estudante.ufla.br>
2023-03-03 17:12:48 -0300

    corrigido

--- get_id.php
+++ get_id.php
@@ -1,23 +1,23 @@
 <?php
-header('Access-Control-Allow-Origin: *');//Permite que o arquivo seja acessado de qualquer origem
-require_once ('config.php');//Requer o arquivo de configuração
+header('Access-Control-Allow-Origin: *');
+require_once ('config.php');
 
-// Recebe o id do motorista,$_POST recupera valores enviados via método POST
-$id = $_POST['id'];
+
+$id = $_POST['id'];//Recebe o id do motorista
 
 if(empty($id)){
     echo json_encode(["message"=>"Sem id válido"]);
 }else {
     $sql = "SELECT id, nome, cpf, endereco, veiculo,telefone FROM motoristas WHERE id = $id";
     $response = $connection -> query($sql);
-    $rows = array();//Cria um array para armazenar os motoristas
+    $rows = array();
 
     //Verifica se há motoristas com o id informado
     if($response -> num_rows > 0){
         foreach($response as $r){//Percorre o array de motoristas
-            $rows= $r;//$rows conterá todos os elementos em $response, na mesma ordem
+            $rows = $r;
         }
-        echo json_encode($rows);//Retorna o motorista em formato JSON
+        echo json_encode($rows);
     }else{
         echo json_encode(["message"=>"Nenhum motorista encontrado com esse id"]);
     }
